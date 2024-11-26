0a949460ef80da81898fee70dd3c4d09898d2217
nice-witch <78809819+nice-witch@users.noreply.github.com>
2024-11-26 23:10:52 +0500

    add modal component

---
+++ src/components/ModalComponent.vue
@@ -0,0 +1,90 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import FolderTreeComponent from "./FolderTreeComponent.vue";
+import { mockFolders } from "@/mockData";
+
+interface Props {
+  title: string;
+}
+const props = defineProps<Props>();
+
+const emit = defineEmits<{
+  (e: "close"): void;
+  (e: "select", folder: string): void;
+}>();
+
+const selectFolder = ref<string | null>();
+
+const chooseFolder = (folder: number) => {
+  selectFolder.value = `Выбранная папка ID: ${folder}`;
+};
+</script>
+
+<template>
+  <Teleport to="body">
+    <div class="overlay">
+      <div class="modal-container">
+        <h2>{{ title }}</h2>
+        <div class="folder-container">
+          <FolderTreeComponent
+            :folders="mockFolders"
+            @chooseFolder="chooseFolder"
+          />
+        </div>
+        <div class="button-container">
+          <button @click="$emit('select', selectFolder)">Ок</button>
+          <button @click="$emit('close')">Закрыть</button>
+        </div>
+      </div>
+    </div>
+  </Teleport>
+</template>
+
+<style scoped>
+.overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  justify-content: center;
+  width: 100vw;
+  height: 100vh;
+  background: #00000080;
+}
+
+.modal-container {
+  display: flex;
+  flex-direction: column;
+  gap: 20px;
+  align-self: center;
+  width: 390px;
+  padding: 20px;
+  border-radius: 15px;
+  background-color: #eee;
+}
+
+.button-container {
+  display: flex;
+  justify-content: space-between;
+}
+
+.details {
+  padding-left: 15px;
+}
+
+.details p:hover,
+.folder-container summary:hover {
+  cursor: pointer;
+}
+/*
+.folder-container p:focus,
+.folder-container summary:focus {
+  color: red;
+}
+*/
+
+.choose-folder {
+  font-weight: bolder;
+  color: blue;
+}
+</style>
