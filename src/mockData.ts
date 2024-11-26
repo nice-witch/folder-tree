de05ab348ca61213aaa327bd2920b1055f0e5eaa
nice-witch <78809819+nice-witch@users.noreply.github.com>
2024-11-26 23:15:06 +0500

    correct mock data

--- src/mockData.ts
+++ src/mockData.ts
@@ -3,19 +3,78 @@ export const mockFolders = [
     id: 1,
     name: "Папка 1",
     children: [
-      { id: 2, name: "Папка 1.1", children: [] },
       {
-        id: 3,
+        id: 2,
+        name: "Папка 1.1",
+        children: [
+          { id: 3, name: "Папка 1.1.1", children: [] },
+          { id: 4, name: "Папка 1.1.2", children: [] },
+        ],
+      },
+      {
+        id: 5,
         name: "Папка 1.2",
         children: [
           {
-            id: 4,
+            id: 6,
             name: "Папка 1.2.1",
-            children: [],
+            children: [{ id: 7, name: "Папка 1.2.1.1", children: [] }],
           },
+          { id: 8, name: "Папка 1.2.2", children: [] },
         ],
       },
     ],
   },
-  { id: 5, name: "Папка 2", children: [] },
+  {
+    id: 9,
+    name: "Папка 2",
+    children: [
+      { id: 10, name: "Папка 2.1", children: [] },
+      {
+        id: 11,
+        name: "Папка 2.2",
+        children: [
+          {
+            id: 12,
+            name: "Папка 2.2.1",
+            children: [
+              { id: 13, name: "Папка 2.2.1.1", children: [] },
+              { id: 14, name: "Папка 2.2.1.2", children: [] },
+            ],
+          },
+          { id: 15, name: "Папка 2.2.2", children: [] },
+        ],
+      },
+    ],
+  },
+  {
+    id: 16,
+    name: "Папка 3",
+    children: [
+      {
+        id: 17,
+        name: "Папка 3.1",
+        children: [
+          {
+            id: 18,
+            name: "Папка 3.1.1",
+            children: [
+              { id: 19, name: "Папка 3.1.1.1", children: [] },
+              { id: 20, name: "Папка 3.1.1.2", children: [] },
+            ],
+          },
+        ],
+      },
+      { id: 21, name: "Папка 3.2", children: [] },
+      {
+        id: 22,
+        name: "Папка 3.3",
+        children: [
+          { id: 23, name: "Папка 3.3.1", children: [] },
+          { id: 24, name: "Папка 3.3.2", children: [] },
+        ],
+      },
+    ],
+  },
+  { id: 25, name: "Папка 4", children: [] },
 ];
