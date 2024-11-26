dd350068961a4961e729615f10fd82e8faf1c049
nice-witch <78809819+nice-witch@users.noreply.github.com>
2024-11-26 23:11:44 +0500

    add base component

---
+++ src/components/BaseComponent.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import ModalComponent from "./ModalComponent.vue";
+
+const isOpenModal = ref(false);
+
+const openModal = () => (isOpenModal.value = true);
+const closeModal = () => (isOpenModal.value = false);
+const selectFolder = ref<string | null>();
+
+const select = (folder: string) => {
+  closeModal();
+  selectFolder.value = folder;
+};
+</script>
+
+<template>
+  <button @click="openModal">Открыть</button>
+  <p v-if="selectFolder">{{ selectFolder }}</p>
+
+  <ModalComponent
+    v-if="isOpenModal"
+    title="Дерево папок"
+    @select="select"
+    @close="closeModal"
+  />
+</template>
+
+<style scoped></style>
