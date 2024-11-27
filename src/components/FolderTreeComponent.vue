93ad7db52cdd9e5b2a65f5194b0d226c93fa4aef
nice-witch <78809819+nice-witch@users.noreply.github.com>
2024-11-27 09:58:34 +0500

    add style for choose folder

--- src/components/FolderTreeComponent.vue
+++ src/components/FolderTreeComponent.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch } from "vue";
+import { ref } from "vue";
 
 interface Folder {
   id: number;
@@ -15,7 +15,10 @@ const emit = defineEmits<{
   (e: "chooseFolder", folder: number): void;
 }>();
 
+const chooseFolder = ref<number>();
+
 const handleFolderClick = (folderId: number) => {
+  chooseFolder.value = folderId;
   emit("chooseFolder", folderId);
 };
 </script>
@@ -23,7 +26,10 @@ const handleFolderClick = (folderId: number) => {
 <template>
   <div v-for="folder in folders" :key="folder.id" class="details">
     <details v-if="folder.children.length">
-      <summary @click="handleFolderClick(folder.id)">
+      <summary
+        @click="handleFolderClick(folder.id)"
+        :class="{ selected: chooseFolder === folder.id }"
+      >
         {{ folder.name }}
       </summary>
       <FolderTreeComponent
@@ -31,14 +37,30 @@ const handleFolderClick = (folderId: number) => {
         @chooseFolder="handleFolderClick"
       />
     </details>
-    <p v-else @click="handleFolderClick(folder.id)">
+    <p
+      v-else
+      @click="handleFolderClick(folder.id)"
+      :class="{ selected: chooseFolder === folder.id }"
+    >
       {{ folder.name }}
     </p>
   </div>
 </template>
 
 <style scoped>
-.details {
+.details,
+.details p {
   padding-left: 15px;
+}
+
+.details summary,
+.details p {
+  cursor: pointer;
+}
+
+.details summary.selected,
+p.selected {
+  font-weight: bold;
+  color: #007bff;
 }
 </style>
